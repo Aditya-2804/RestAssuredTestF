ba9610d89a4513b58ae2fd498796d1c77fdba3c1
Aditya-2804 <166024647+Aditya-2804@users.noreply.github.com>
2025-05-31 16:54:31 +0530

    Update LogInTest.java Added wait

--- src/test/java/LogInAPITest/LogInTest.java
+++ src/test/java/LogInAPITest/LogInTest.java
@@ -10,12 +10,13 @@ public class LogInTest {
 
 
     @Test
-    public void LogInTest1(){
+    public void LogInTest1() throws Exception{
 
         System.out.println("LogIn Test");
 
         WebDriver driver = new EdgeDriver();
-        driver.get("https://www.google.co.in/")
+        driver.get("https://www.google.co.in/");
+        Thread.sleep(5*1000);
         String str  =  get("https://fakestoreapi.com/products").getBody().asString();
         System.out.println(str);
 
