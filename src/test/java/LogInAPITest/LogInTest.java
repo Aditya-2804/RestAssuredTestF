8587d13165eaec9799ce8ba8b79b42bbca6d1488
Aditya-2804 <iamplaceholderaccont@gmail.com>
2025-06-17 17:23:30 +0530

    Added WebDriver to navigate and Search

--- src/test/java/LogInAPITest/LogInTest.java
+++ src/test/java/LogInAPITest/LogInTest.java
@@ -4,6 +4,10 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
 import io.restassured.RestAssured;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class LogInTest {
@@ -14,8 +18,19 @@ public class LogInTest {
 
         System.out.println("LogIn Test");
 
+
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.google.com/");
         String str  =  get("https://fakestoreapi.com/products").getBody().asString();
+        System.out.println(driver.getCurrentUrl());
+        driver.findElement(By.id("APjFqb")).sendKeys("Aditya Naik");
+        driver.findElement(By.id("APjFqb")).sendKeys(Keys.ENTER);
+
+        Thread.sleep(5*1000);
+        System.out.println(driver.getCurrentUrl());
         System.out.println(str);
+
+        driver.quit();
 
 
     }
